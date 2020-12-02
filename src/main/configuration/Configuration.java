1dd722520723a3fadba543dabb7230f43d090e93
tobiasEmmerling <tobi1emmerling@gmail.com>
2020-12-02 09:38:21 +0100

    addes right path for data

--- src/main/configuration/Configuration.java
+++ src/main/configuration/Configuration.java
@@ -14,4 +14,10 @@ public enum Configuration {
     public static FileReader fileReader = new FileReader();
     public static AES aes = new AES();
 
+    // i/o
+    public final String fileSeparator = System.getProperty("file.separator");
+    public final String userDirectory = System.getProperty("user.dir");
+    public final String lineSeparator = System.getProperty("line.separator");
+    public final String dataDirectory = userDirectory + fileSeparator + "resources" + fileSeparator + "passenger_baggage.txt" + fileSeparator;
+
 }
