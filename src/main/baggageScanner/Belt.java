dd0f5503a90fe170d505ad15502c31ce30cb52da
tobiasEmmerling <tobi1emmerling@gmail.com>
2020-12-02 10:21:23 +0100

    changed encrypt decrypt

--- src/main/baggageScanner/Belt.java
+++ src/main/baggageScanner/Belt.java
@@ -1,10 +1,11 @@
 package main.baggageScanner;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Belt {
-    private Queue<Tray> trays = new PriorityQueue<>();
+    private Queue<Tray> trays = new LinkedList<>();
 
 
     public Queue<Tray> getTrays() {
